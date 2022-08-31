d0dc73e5ffdc4cfc79a335409ecb396b3497baad
billyovo <30436689+billyovo@users.noreply.github.com>
2022-08-31 19:38:17 +0800

    update sql 2.0

--- sql/news/get_news_list.sql
+++ sql/news/get_news_list.sql
@@ -2,10 +2,10 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS get_news_list;
 CREATE PROCEDURE `get_news_list`(lim INT,  pg INT)
 BEGIN
-  SELECT ID, title, publish_date FROM news LIMIT lim OFFSET pg ORDER BY publish_date DESC, ID DESC;
+  SELECT ID, title, publish_date FROM news ORDER BY publish_date DESC, ID DESC LIMIT lim OFFSET pg;
   SELECT FOUND_ROWS() AS total;
 END //
 
 /*
---CALL get_news_list(0, 0);
+--CALL get_news_list(5, 0);
 */
