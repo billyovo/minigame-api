8749e387213951684fbed710050628db062286a5
billyovo <30436689+billyovo@users.noreply.github.com>
2023-04-08 17:43:34 +0800

    add font

--- utils/image.js
+++ utils/image.js
@@ -1,19 +1,19 @@
 import path from 'path';
-import {createCanvas, loadImage} from 'canvas';
+import {createCanvas, loadImage, registerFont} from 'canvas';
 import fs from 'fs';
 export async function makeBanner(eventName, eventTime){
     const canvas = createCanvas(600,200);
     const ctx = canvas.getContext("2d");
-
+    registerFont('./assets/font.ttf', {family: "gothicx"})
     const base = await loadImage(path.resolve(".", "./assets/banner.png"));
     ctx.drawImage(base, 0,0);
 
-    ctx.font = '65px Arial bold';
+    ctx.font = '65px gothicx';
     ctx.fillStyle = "white"
     ctx.textAlign = "center"
     ctx.fillText(eventName, base.width/2, (base.height/2)+15);
     
-    ctx.font = '20px Arial bold';
+    ctx.font = '20px gothicx';
     ctx.textAlign = "left";
     ctx.fillText(eventTime, base.width-150, base.height-22)
     const buffer = canvas.toBuffer("image/png");
