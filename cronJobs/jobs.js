ec692d505be2b8583b99dfef9d9b2303a93d5ec9
billyovo <30436689+billyovo@users.noreply.github.com>
2023-04-08 17:29:08 +0800

    add banner

--- cronJobs/jobs.js
+++
@@ -1,8 +0,0 @@
-import { updateEvents, eventsDateMap } from "../utils/getEventSchedule.js";
-import { CronJob } from "cron";
-import { makeBanner } from "../utils/image.js";
-const scheduleCheckEvent = new CronJob('1 0 * * *', async function() {
-    updateEvents();
-    await makeBanner(eventsDateMap.nearest.title, eventsDateMap.nearest.date)
-}, null, true, 'Asia/Taipei');
-scheduleCheckEvent.start();
